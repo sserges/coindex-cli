fe52eaba8d9a250aaeccb57c21747e0bdf5f3d27
sserges <agounkeserge@gmail.com>
2020-09-14 18:50:08 +0000

    KeyManager class added

---
+++ commands/key.js
@@ -0,0 +1,13 @@
+const key = {
+    set() {
+        console.log('Hello from set 1');
+    },
+    show() {
+        console.log('Hello from show 2');
+    },
+    remove() {
+        console.log('Hello from remove 3');
+    }
+}
+
+module.exports = key;
