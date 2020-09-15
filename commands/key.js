393c4f8877550cfe8a96961e394f775b9607d40f
sserges <agounkeserge@gmail.com>
2020-09-15 11:07:29 +0000

    set method of keyManager done

--- commands/key.js
+++ commands/key.js
@@ -1,6 +1,27 @@
+const inquirer = require('inquirer');
+const colors = require('colors');
+
+const KeyManager = require('../lib/KeyManager');
+const  { isRequired }  = require('../utils/validation');
+
 const key = {
-    set() {
-        console.log('Hello from set 1');
+    async set() {
+        const keyManager = new KeyManager();
+        const input = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'key',
+                message: 'Enter API Key '.green + 'https://nomics.com',
+                validate: isRequired
+            }
+        ]);
+
+        const key = keyManager.setKey(input.key);
+
+        if (key) {
+            console.log('API Key Set'.blue);
+        }
+
     },
     show() {
         console.log('Hello from show 2');
